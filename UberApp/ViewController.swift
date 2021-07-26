457129d75e180a68c77a80a52326fa5c0e780c9d
Adam Januszewski <Adpjan@att.net>
2021-07-26 12:14:05 -0400

    Furthering the development of Firebase

--- UberApp/ViewController.swift
+++ UberApp/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var driverLabel: UILabel!
+    
+    var signUpMode = true
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func topTapped(_ sender: UIButton) {
+    }
+    @IBAction func bottomTapped(_ sender: UIButton) {
+        if signUpMode {
+            topButton.setTitle("Log In", for: .normal)
+            bottomButton.setTitle("Switch to Sign Up", for: .normal)
+            riderLabel.isHidden = true
+            driverLabel.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMode = false
+        } else {
+            topButton.setTitle("Sign Up", for: .normal)
+            bottomButton.setTitle("Switch to Log In", for: .normal)
+            riderLabel.isHidden = false
+            driverLabel.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMode = true
+        }
+        
+    }
+    
+    
+    
 
 }
 
